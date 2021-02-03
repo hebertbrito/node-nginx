82073fed983e089a0b16aff07702ba30c6676453
hebert felipe <hebertfelipe.97@outlook.com.br>
2021-02-03 14:58:05 -0300

    push

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express'
 
 const app = express()
-const port = process.env.PORT || 3080
+const port = process.env.PORT || 4020
 app.get("/api", async (req, resp) => {
-    resp.json("server1")
+    resp.send("<h1>some html</h1>")
 })
 
 app.listen(port, ()=>{
